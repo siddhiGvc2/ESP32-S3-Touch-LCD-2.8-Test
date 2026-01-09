dc48bb7cd9fbc17659ebec31bc98fea26496b49d
siddhiGvc2 <sd@gvc.in>
2026-01-09 11:29:50 +0530

    command & nvs file added

--- main/GVC/inc/externVars.h
+++ main/GVC/inc/externVars.h
@@ -1,2 +1,10 @@
+// vars for kwikpay 
+
+// vars for AAC
 extern int track_id,Total_Tracks,CurrentTrack;  
+
+
+// vars for traffic
+
+// vars for WAM
 
---
+++ main/GVC/src/command.c
@@ -0,0 +1,21 @@
+void AnalyseGeneralCommands (char* pkt) {
+    char payload[32];
+    int track_id;
+}
+
+void AnalyseKwilpayCommands (char* pkt) {
+    char payload[32];
+    int track_id;
+}
+
+void AnalyseVendingCommands (char* pkt) {
+    char payload[32];
+    int track_id;
+}
+
+void AnalyseWAMCommands (char* pkt) {
+    char payload[32];
+    int track_id;
+}
+
+
