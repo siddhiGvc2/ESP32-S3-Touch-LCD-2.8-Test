5bd90841b8a7512be4b39e2908390f9224e22427
siddhidhamnaskar <siddhidhamnaskar64@gmail.com>
2026-01-10 13:50:44 +0530

    minor changes

--- main/GVC/src/command.c
+++ main/GVC/src/command.c
@@ -14,48 +14,48 @@ void SendReply(char* InputVia, char* data)
 
 
 
-void AnalyseGeneralCommands (char* InputVia,char* pkt) {
+void AnalyseGeneralCommands (char* InputVia,char* rx_buffer) {
     char payload[800];
     int track_id;
-     if(strncmp(pkt, "*SN?#", 5) == 0){
+     if(strncmp(rx_buffer, "*SN?#", 5) == 0){
         sprintf(payload, "*SN,%s,%s,%s#",SNuserName,SNdateTime,SerialNumber); //actual when in production
         ESP_LOGI(TAG,"%s",payload);
         SendReply(InputVia,payload);
     }
-    else if(strncmp(pkt, "*URL?#", 6) == 0){
+    else if(strncmp(rx_buffer, "*URL?#", 6) == 0){
         sprintf(payload,"*URL,%s,%s,%s#",URLuserName,URLdateTime,FOTA_URL);
         ESP_LOGI(TAG,"%s",payload);
         SendReply(InputVia,payload);
     }
-    else if(strncmp(pkt,"*RSSI?#",7)==0)
+    else if(strncmp(rx_buffer,"*RSSI?#",7)==0)
     {
     sprintf(payload,"*RSSI,%d",RSSI);
     ESP_LOGI(TAG,"%s",payload);
     SendReply(InputVia,payload);
     }
-    else if(strncmp(pkt, "*SSID?#", 7) == 0){
+    else if(strncmp(rx_buffer, "*SSID?#", 7) == 0){
         sprintf(payload, "*SSID,%s,%s,%d,%s,%s,%s#",SSuserName,SSdateTime,WiFiNumber,WIFI_SSID_1,WIFI_SSID_2,WIFI_SSID_3); 
         ESP_LOGI(TAG,"%s",payload);
         SendReply(InputVia,payload);
        
     }
-     else if(strncmp(pkt, "*FW?#", 5) == 0){
+     else if(strncmp(rx_buffer, "*FW?#", 5) == 0){
         ESP_LOGI(TAG,"*%s#",FWVersion);
         sprintf(payload, "*FW:%s#",FWVersion);
         SendReply(InputVia,payload);
      }
-     else if(strncmp(pkt, "*SIP?#", 6) == 0){
+     else if(strncmp(rx_buffer, "*SIP?#", 6) == 0){
         sprintf(payload,"*SIP,%s,%s,%s,%d#",SIPuserName,SIPdateTime,server_ip_addr,
                                          sp_port );
         SendReply(InputVia,payload);
        
     }
-      else if(strncmp(pkt, "*MIP?#", 6) == 0){
+      else if(strncmp(rx_buffer, "*MIP?#", 6) == 0){
         sprintf(payload,"*MIP,%s,%s,%s,%d#",MIPuserName,MIPdateTime,mqtt_uri,MipNumber);
         SendReply(InputVia,payload);
        
     }
-    else if (strncmp(pkt, "*ERASE?", 7) == 0){
+    else if (strncmp(rx_buffer, "*ERASE?", 7) == 0){
         sprintf(payload,"*ERASE,%s,%s,%s#",ERASEuserName,ERASEdateTime,ErasedSerialNumber); 
         SendReply(InputVia, payload);
     }
