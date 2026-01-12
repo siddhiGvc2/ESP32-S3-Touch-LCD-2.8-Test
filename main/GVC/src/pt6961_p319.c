f8df2524febcfbd8d4797b1474592ee888083e30
siddhidhamnaskar <siddhidhamnaskar64@gmail.com>
2026-01-12 12:34:55 +0530

    WAM COMMANDS ADDED

--- main/GVC/src/pt6961_p319.c
+++ main/GVC/src/pt6961_p319.c
@@ -1,34 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "freertos/event_groups.h"
-#include "esp_system.h"
-#include "esp_wifi.h"
-#include "esp_mac.h"
-#include "esp_event.h"   
-#include "esp_log.h"
-#include "nvs_flash.h"
-#include "driver/gpio.h"
-#include "lwip/err.h"
-#include "lwip/sys.h"
-#include "lwip/sockets.h"
-#include "esp_http_client.h"
-#include "esp_https_ota.h"
-#include "esp_timer.h"
-#include "esp_ota_ops.h"
-#include "driver/uart.h"
-#include "esp_netif.h"
-#include "rom/ets_sys.h"
-#include "esp_smartconfig.h"
-#include <sys/socket.h>
-#include <sys/socket.h>
-#include <netdb.h>
-#include <arpa/inet.h>
-
-#include "externVars.h"
-#include "calls.h"
+#include "app.h"
 
 #define HIGH 1
 #define LOW 0
